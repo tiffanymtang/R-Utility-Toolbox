b77f9cedcf766a7151a8c3656970465ef3cb4893
Tiffany Tang <tmtang15@gmail.com>
2021-07-08 10:41:43 -0700

    add additional args to subchunkify

--- rmd_functions.R
+++ rmd_functions.R
@@ -1,6 +1,7 @@
 # rmarkdown helper functions
 
-subchunkify <- function(g, i, fig_height = 12, fig_width = 10, caption = "''") {
+subchunkify <- function(g, i, fig_height = 12, fig_width = 10, caption = "''",
+                        other_args = "") {
   ####### Function Description ########
   # function to allow for multiple plots of different sizes and captions
   # within a single R code chunk
@@ -14,14 +15,21 @@ subchunkify <- function(g, i, fig_height = 12, fig_width = 10, caption = "''") {
   # - fig_width = width of figure
   # - caption = figure caption; should be within surrounded by two sets of
   #     quotes, e.g., "'This is a valid caption.'"
+  # - other_args = other arguments to pass to R code chunk header
   ####### 
   
   g_deparsed <- paste0(deparse(function() {g}), collapse = '')
   
+  if (!identical(other_args, "")) {
+    if (!startsWith(other_args, ",")) {
+      other_args <- paste0(", ", other_args)
+    }
+  }
+  
   sub_chunk <- paste0("
-  `","``{r sub_chunk_", i, 
+  `","``{r subchunk_", i, 
                       ", fig.height=", fig_height, ", fig.width=", fig_width, 
-                      ", fig.cap=", caption,", echo=FALSE}",
+                      ", fig.cap=", caption,", echo=FALSE", other_args, "}",
                       "\n(", g_deparsed, ")()",
                       "\n`","``
   ")
